dc421582f4dd178a0e96518516767b928685a7b7
Scott <scottmail88@gmail.com>
2017-10-19 19:44:03 +0000

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+(function() {
+    function config($locationProvider, $stateProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+             });
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                controller: 'HomeCtrl as home',
+                templateUrl: '/templates/home.html'
+            });
+    }
+    
+    angular
+        .module('blocTime', ['ui.router', 'firebase'])
+        .config(config);
+})();
